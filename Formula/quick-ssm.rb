0b5b4ef45dd1bb382d589f80175542757842f728
Zach Gates <zgates@bevel.work>
2025-09-11 13:20:44 -0400

    fix: removed v prefix

--- Formula/quick-ssm.rb
+++ Formula/quick-ssm.rb
@@ -1,7 +1,7 @@
 class QuickSsm < Formula
   desc "Quickly connect to AWS servers from the cli. No GUI or ssh needed"
   homepage "https://github.com/bevelwork/quick_ssm"
-  url "https://github.com/bevelwork/quick_ssm/archive/refs/tags/v1.26.20250911.tar.gz"
+  url "https://github.com/bevelwork/quick_ssm/archive/refs/tags/1.26.20250911.tar.gz"
   sha256 "ffb0667ce50108bc88014f392d4e1c13d80b795bd1a771efb967c1846ab8c5ed"
   license "Apache-2.0"
 
