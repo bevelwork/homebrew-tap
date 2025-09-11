144681402bae9d8de5505079163e7d2f1349ec09
GitHub Action <action@github.com>
2025-09-11 19:55:22 +0000

    Bump quick-ssm to 1.1.20250911

--- Formula/quick-ssm.rb
+++ Formula/quick-ssm.rb
@@ -1,8 +1,8 @@
 class QuickSsm < Formula
   desc "Quickly connect to AWS servers from the cli. No GUI or ssh needed"
   homepage "https://github.com/bevelwork/quick_ssm"
-  url "https://github.com/bevelwork/quick_ssm/archive/refs/tags/1.27.20250911.tar.gz"
-  sha256 "8a60dbd6c2d52b21f3d27b5bdb79a4b35d0384826df3e2f99d147019be538ffc"
+  url "https://github.com/bevelwork/quick_ssm/archive/refs/tags/1.1.20250911.tar.gz"
+  sha256 "2b68c09a2253341b4e66915f7cf573eb1cc4a25185d77f3b5240d00a5b4dac3b"
   license "Apache-2.0"
 
   depends_on "go" => :build
