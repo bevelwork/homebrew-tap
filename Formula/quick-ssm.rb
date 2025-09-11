a350d481eb75f4f0f4c4df4e880eaf20e05da567
Zach Gates <zgates@bevel.work>
2025-09-11 10:58:02 -0400

    feat: adding quick-ssm formula

---
+++ Formula/quick-ssm.rb
@@ -0,0 +1,17 @@
+class QuickSsm < Formula
+  desc "Quickly connect to AWS servers from the cli. No GUI or ssh needed."
+  homepage "https://github.com/bevelwork/quick_ssm"
+  url "https://github.com/bevelwork/quick_ssm/archive/refs/tags/1.16.20250911.tar.gz"
+  sha256 "bc5d921729e22e9adac0321950fe85e467f9922c6ea0d704f49131ff95ff2b4b"
+  license "NOASSERTION"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system "false"
+  end
+end
