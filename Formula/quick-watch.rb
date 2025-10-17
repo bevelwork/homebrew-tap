fa3ee1d2ef06c9225fb3715cec0737620509b25e
GitHub Action <action@github.com>
2025-10-17 05:30:32 +0000

    Bump quick-watch to 1.11.20251016

--- Formula/quick-watch.rb
+++ Formula/quick-watch.rb
@@ -1,8 +1,8 @@
 class QuickWatch < Formula
   desc "Simple Go CLI tool for monitoring URLs and services with configurable alerts and webhook notifications"
   homepage "https://github.com/bevelwork/quick_watch"
-  url "https://github.com/bevelwork/quick_watch/archive/refs/tags/1.4.20251015.tar.gz"
-  sha256 "477cf30a959e92822e70431885aadabbc62a18a6ec9c373b089f93b2f5fea2af"
+  url "https://github.com/bevelwork/quick_watch/archive/refs/tags/1.11.20251016.tar.gz"
+  sha256 "446e34a195adc2061de6c4aeecb78049dd129e4684968904ee48e9b0de7c9b28"
   license "Apache-2.0"
 
   depends_on "go" => :build
