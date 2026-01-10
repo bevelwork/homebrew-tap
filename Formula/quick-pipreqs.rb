3b1e177662d0feca58a755a38b195213ac22de1b
GitHub Action <action@github.com>
2026-01-10 05:35:09 +0000

    Bump quick-pipreqs to

--- Formula/quick-pipreqs.rb
+++ Formula/quick-pipreqs.rb
@@ -1,8 +1,8 @@
 class QuickPipreqs < Formula
   desc "Quickly generate requirements.txt from Python projects"
   homepage "https://github.com/bevelwork/quick_pipreqs"
-  url "https://github.com/bevelwork/quick_pipreqs/archive/refs/tags/1.8.20251007.tar.gz"
-  sha256 "7715e00ac77d2afe987c75f1080b8f61c08f6e5eb7110832e6d71f87fa9c5216"
+  url "https://github.com/bevelwork/quick_pipreqs/archive/refs/tags/.tar.gz"
+  sha256 "172dbbf468f2d83896081a22c4d65964cd0fa3dfda7926bfbd6da8b7d4d91d75"
   license "Apache-2.0"
 
   depends_on "go" => :build
